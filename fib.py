087033d5e86e5a7c3728a272cee23e390f4a03b0
Joseph Thomas Hall <josephthomashall@csm-wl-dhcp-226-139.mines.edu>
2019-11-21 15:28:08 -0700

    fib1

---
+++ fib.py
@@ -0,0 +1,17 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,9):
+
+        fib_n = finbs[1] + fibs[i-1]
+        fibs.append(fib-n)
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
